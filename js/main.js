630d98bf8764e2134ffe2cabf9e019b1b827568c
Eric-Franklin79 <pickle797900@verizon.net>
2015-02-09 21:21:05 -0500

    first

--- js/main.js
+++ js/main.js
@@ -1,41 +1,122 @@
 window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
     
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
+   "use strict";
     
-    "use strict";
-    
-    Fighter = function(index, game){
-        
-    }
-    Fighter.prototype.det = function() {
+    Tux = function( game){
+    	    this.x = 100;
+    	    this.y = 400;
+    	    this.game = game;
+    	    this.tuxMan = game.add.sprite( this.x, this.y, 'tux', 'Stance.png');
+    	    game.physics.enable(this.tuxMan, Phaser.Physics.ARCADE);
+    	    this.tuxMan.anchor.setTo(0.5, 0.5);
+    	    this.tuxMan.body.collideWorld = true;
+    	    this.tuxMan.animations.add('punch', ['punchMid.png', 'punch.png', 'punchRe.png', 'Stance.png'], 10);
+    	    this.tuxMan.animations.add('kick', ['kcikStart.png', 'kick.png', 'kick.png','kickDown.png', 'Stance.png'], 10);
+    	    this.tuxMan.animations.add('dodge', ['dodge.png'], 5, true);
     	    
+    	    this.tuxMan.maxHealth = 200;
     }
-    Fighter.prototype.fight = function() {
-    
+    Tux.prototype.det = function() {
+    	    this.tuxMan.destroy();
+    }
+    Tux.prototype.fight = function() {
+    	    if(dodge.isDown){
+    	    	   this.tuxMan.animations.play('dodge');
+    	    }
+    	    else if(kick.isDown){
+    	    	   this.tuxMan.animations.play('kick');    
+    	    }
+    	    else if(punch.isDown){
+    	    	   this.tuxMan.animations.play('punch');
+    	    }
+    	    else{
+    	    	this.tuxMan.loadTexture('tux', 'Stance.png');    
+    	    }
+    }
+    Tux.prototype.move = function() {
+    	    if(cursors.left.isDown){
+    	    	    this.tuxMan.body.velocity.x = -200;
+    	    }
+    	    else if(cursors.right.isDown){
+    	    	    this.tuxMan.body.velocity.x = 200;
+    	    }
+    	    else{
+    	    	    this.tuxMan.body.velocity.x = 0;
+    	    	    //load default texture?
+    	    	    //try to get acceleration and deceleration
+    	    }
+    	    if(cursors.up.isDown){
+    	    	    this.tuxMan.body.velocity.y = -200;
+    	    }
     }
     
-    var game = new Phaser.Game( 800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1300, 755, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-       // game.load.image( 'park', 'assets/.png' );
+    	     game.load.atlasJSONHash( 'tux', 'assets/tux.png', 'assets/tux.json' );
+    	     game.load.image( 'diner', 'assets/diner.png' );
+       
         
     }
-  
+    var tuxMan;
+    var background;
+    var Tux;
+    var roundTime = 0;
+    var timer;
+    var gameTime;
+    var rountCount = 1;
+    var cursors;
+    var timeText;
+    var pause;
+    var punch;
+    var kick;
+    var dodge;
+    var tuxMan;
     
     function create() {
-       
+    	    background = game.add.tileSprite(0,0, 1300, 755, 'diner');
+    	    
+    	    
+    	    
+    	    tuxMan = new Tux(game);
+    	    
+    	    //Time for rounds
+    	   // var styleT = { font: "35px Verdana", fill: "#FFFFFF", align: "center" };
+    	  //  timeText = game.add.text(300, 200, "Click to Start", styleT);
+    	    
+    	    cursors = game.input.keyboard.createCursorKeys();
+    	    game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
+    	    pause = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    	    punch = game.input.keyboard.addKey(Phaser.Keyboard.Q);
+    	    kick = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    	    dodge = game.input.keyboard.addKey(Phaser.Keyboard.E);
     }
     
     function update() {
-    	
+    	    gameTime = new Date().getTime();
+    	    tuxMan.move();
+    	    tuxMan.fight();
+    }
+    function updateTimer(){
+    	    timer = 90 - Math.floor((gameTime-roundTime)*.001);
+    	    timeText.x = game.world.centerX;
+    	    timeText.y = 10;
+    	    timeText.setText(String(timer));
+    	    if(timer < 0){roundOver()} 
+    }
+    function roundOver(){
+    	    if(roundCount === 3){
+    	    	    //game over
+    	    }
+    	    else{
+    	    	    newRound();
+    	    	    rountCount++;
+    	    }
+    }
+    function newRound(){
+    	    roundTime = new Date().getTime();
+    	//reset sprites
+    	//reset health
+    	//reset timer
     }
 };
