b921e625768e40fe91dd29f1939b990ab7936611
Eric-Franklin79 <pickle797900@verizon.net>
2015-02-11 16:05:43 -0500

    almost done

--- js/main.js
+++ js/main.js
@@ -122,16 +122,17 @@ window.onload = function() {
     	     game.load.atlasJSONHash( 'tux', 'assets/tux.png', 'assets/tux.json' );
     	     game.load.image( 'diner', 'assets/diner.png' );
     	     game.load.image( 'health', 'assets/health.png' );
+       	     game.load.image( 'gameOver', 'assets/gameOver.png' );
         
     }
     var tuxMan;
     var Tux;
-    var roundTime = new Date().getTime();;
+    var roundTime = new Date().getTime();
     var timer = 90;
     var gameTime;
     var roundCount = 1, winCount = 0;
     var cursors;
-    var timeText, roundTimeText;
+    var timeText, roundTimeText, roundText, countText, punchText, kickText, dodgeText, moveText;
     var pause;
     var punch;
     var kick;
@@ -141,6 +142,8 @@ window.onload = function() {
     var dude;
     var Fighter;
     var fighterHit = false, dudeFighting = false, tuxFighting = false;
+    var end = false, betweenR = true, start = false;
+    
     
     function create() {
     	    game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -153,12 +156,16 @@ window.onload = function() {
     	    
     	    //Create all the text that we need
     	    var styleT = { font: "bold 50px Verdana", fill: "#000000", align: "center" };
-    	    roundTimeText = game.add.text(550, 10, String(timer), styleT);
+    	    roundTimeText = game.add.text(600, 10, String(timer), styleT);
+    	    var styleR = { font: "bold 20px Verdana", fill: "#A4A4A4", align: "center" };
+    	    roundText = game.add.text(590, 60, "Round: " + String(roundCount), styleR);
+    	    var styleC = { font: "bold 30px Verdana", fill: "#F2F2F2", align: "center" };
+    	    countText = game.add.text(550, 280, "Click To Start", styleC);
     	    var styleH = { font: "20px Verdana", fill: "#000000", align: "center" };
-    	    var punchText = game.add.text(350, 175, "Q - Punch", styleH);
-    	    var kickText = game.add.text(500, 175, "W - Kick", styleH);
-    	    var dodgeText = game.add.text(650, 175, "E - Dodge", styleH);
-    	    var moveText = game.add.text(800, 175, "<--/--> - move left/right", styleH);
+    	    punchText = game.add.text(350, 175, "Q - Punch", styleH);
+    	    kickText = game.add.text(500, 175, "W - Kick", styleH);
+    	    dodgeText = game.add.text(650, 175, "E - Dodge", styleH);
+    	    moveText = game.add.text(800, 175, "<--/--> - move left/right", styleH);
     	    
     	    //Create the keys that we need to use cursors, space, Q, W, E
     	    cursors = game.input.keyboard.createCursorKeys();
@@ -171,51 +178,102 @@ window.onload = function() {
     	    tuxMan = new Tux(game);
     	    dude = new Fighter()
     	    
-    	    
+    	    game.input.onDown.add(startGame, this);
     }
     
     function update() {
-    	    gameTime = new Date().getTime();
-    	    if(game.physics.arcade.collide(tuxMan.tuxMan, dude.fighter)){
-    	    	hit();	    
-    	    }
-    	    
-    	    tuxMan.move();
-    	    tuxMan.fight();
-    	    dude.update();
-    	    updateTimer();
+    	    if(start){
+		    if(!end){
+			    if(!betweenR){
+				    gameTime = new Date().getTime();
+				    if(game.physics.arcade.collide(tuxMan.tuxMan, dude.fighter)){
+					hit();	    
+				    }
+				    
+				    tuxMan.move();
+				    tuxMan.fight();
+				    dude.update();
+				    updateTimer();
+			    }
+			    //In between round adjustments... Count down to start
+			    else{
+			    	    tuxMan.tuxMan.body.velocity.x = 0;
+				    tuxMan.tuxMan.body.velocity.y = 0;
+				    dude.fighter.body.velocity.x = 0;
+			    	    gameTime = new Date().getTime();
+			    	    if(Math.floor((gameTime - roundTime)*.001)%5 === 0){
+			    	    	    countText.setText("Round " + String(roundCount));
+				    }
+				    else if(Math.floor((gameTime - roundTime)*.001)%6 === 0){
+				    	    countText.x = 600;
+				    	    countText.setText("3");
+				    }
+				    else if(Math.floor((gameTime - roundTime)*.001)%7 === 0){
+				    	    countText.x = 600;
+				    	    countText.setText("2");
+				    }
+				    else if(Math.floor((gameTime - roundTime)*.001)%8 === 0){
+				    	    countText.x = 600;
+				    	    countText.setText("1");
+				    }
+				    else if(Math.floor((gameTime - roundTime)*.001)%9 === 0){
+				    	    betweenR = false;
+				    	    countText.setText(" ");
+				    	    countText.x = 550;
+				    	    roundTime = new Date().getTime();
+				    }
+			    }
+		    }
+	    }
     }
+    //Round Timer
     function updateTimer(){
     	    timer = 90 - Math.floor((gameTime-roundTime)*.001);
-    	    roundTimeText.x = game.world.centerX;
+    	    roundTimeText.x = 600;
     	    roundTimeText.y = 10;
     	    roundTimeText.setText(String(timer));
     	    if(timer < 0){roundOver()} 
     }
+    //Check to see
     function roundOver(){
     	    if((roundCount === 3)||(winCount == 2)){
-    	    	    gameOver();
+    	    	    end = true;
+    	    	    //if game is over
+    	    	    if(winCount == 2){
+			    game.world.remove(roundTimeText);
+			    game.world.remove(roundText);
+			    game.world.remove(countText);
+			    game.world.remove(punchText);
+			    game.world.remove(kickText);
+			    game.world.remove(dodgeText);
+			    game.world.remove(moveText);
+			    fighterHealthbar.kill();
+			    healthbar.kill();
+			    dude.die();
+		    }
+		    else{
+			   var background = game.add.tileSprite(0,0, 1300, 755, 'gameOver');
+		    }
     	    }
+    	    //If round is over
     	    else{
-    	    	    newRound();
+    	    	    betweenR = true;
+		    roundTime = new Date().getTime();
+		    tuxMan.tuxMan.body.velocity.x = 0;
+		    tuxMan.tuxMan.body.velocity.y = 0;
+		    dude.fighter.body.velocity.x = 0;
+		    tuxMan.tuxMan.revive(500);
+		    tuxMan.reset();
+		    dude.fighter.revive(500);
+		    dude.reset();
+		    fighterHealthbar.reset(800,25);
+		    healthbar.reset(0, 25);
+		    dude.health = 500;
     	    	    roundCount++;
+    	    	    roundText.setText("Round: " + String(roundCount));
     	    }
     }
-    function newRound(){
-    	    roundTime = new Date().getTime();
-    	    tuxMan.reset();
-    	    dude.reset();
-    	    fighterHealthbar.reset(800,25);
-    	    healthbar.reset(0, 25);
-    }
-    function gameOver(){
-    	    if(winCount == 2){
-    	    	    //win condition
-    	    }
-    	    else{
-    	    	    //loose condition
-    	    }
-    }
+    //Check damage 
     function hit(){
     	 if(dudeFighting){
     	 	healthbar.x -= 20;	 
@@ -228,10 +286,17 @@ window.onload = function() {
     	 	 dude.health -= 20;
     	 	 if(dude.health === 0){
     	 	 	dude.die();
+    	 	 	winCount++;
     	 	 	roundOver();
     	 	 }
     	 }
     	 fighterHit = true;
     	 
+    }
+    //Begining of game function
+    function startGame() {
+    	game.input.onDown.remove(startGame, this);
+    	start = true;
+    	roundTime = new Date().getTime();
     }
 };
